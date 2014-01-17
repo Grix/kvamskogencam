cdd4371abe6e414ec3e697405148176fce143be0
Gitle Mikkelsen <gitlem@gmail.com>
2014-01-17 15:00:03 +0100

    Increment

--- loop.py
+++ loop.py
@@ -4,12 +4,14 @@ import time
 from PIL import Image, ImageDraw
 import RPi.GPIO as GPIO
 
+#Move servo, capture photos and repeat
 try:
-	#Move servo, capture photos and repeat
+	#Init camera
 	i = 10;
 	camera = picamera.PiCamera();
 	camera.resolution = (800, 600);
 	time.sleep(1);
+	#Move servo to the left and capture left.jpg
 	while (i):
 		GPIO.output(pin, GPIO.HIGH);
 		time.sleep(0.0011);
@@ -18,6 +20,7 @@ try:
 		i -= 1;
 	time.sleep(1);
 	camera.capture('left.jpg');
+	#Move servo to the right and capture right.jpg
 	while (i):
 		GPIO.output(pin, GPIO.HIGH);
 		time.sleep(0.0019);
@@ -30,31 +33,37 @@ try:
 	time.sleep(1);
 except:	
 	print "Unexpected error camera/servo:", sys.exc_info()[0]
-    raise
+	raise
 
+#Append timestamp to and compress photos
 try:
-	#Append timestamp to and compress photos
+	#Get timestamp
 	ts = time.strftime("%Y-%m-%d %H:%M:%S");
+	#Open photos for manipulation
 	imageright = Image.open("./right.jpg");
 	imageleft = Image.open("./left.jpg");
 	drawright = ImageDraw.Draw(imageright);
 	drawleft = ImageDraw.Draw(imageleft);
+	#Draw the timestamp
 	drawright.text( (300, 50), ts);
 	drawleft.text( (300, 50), ts);
+	#Save manipulated photos to rightfinal.jpg/leftfinal.jpg
 	imageright.save ('rightfinal.jpg', quality=85);
 	imageleft.save ('leftfinal.jpg', quality=85);
 	time.sleep(1);
 except:	
 	print "Unexpected error photo manipulation:", sys.exc_info()[0]
-    raise
+	raise
 
+#Upload photos to FTP server
 try:
-	#Upload photos to FTP server
+	#Open maniulated images
 	photoleft = open("./leftfinal.jpg", "rb");
 	photoright = open("./rightfinal.jpg", "rb");
-	ftp = FTP('ftp.bitlasers.com', 'kvamskogen@bitlasers.com', 'G0liates_ftp');
+	#Connect to FTP and store
+	ftp = FTP('ftp.bitlasers.com', 'kvamskogen@bitlasers.com', '');
 	ftp.storbinary("STOR right.jpg", photoright);
 	ftp.storbinary("STOR left.jpg", photoleft);
 except:	
 	print "Unexpected error FTP:", sys.exc_info()[0]
-    raise
+	raise
